161df3d47fcbce38d02a00cf25246a35ce1ee892
Hoel Bagard <hoel.bagard@gmail.com>
2022-04-19 13:18:47 +0900

    Added docstring.

--- interval_counter/interval_counter.c
+++ interval_counter/interval_counter.c
@@ -4,7 +4,7 @@
 
 int interval_counter(int start, int end) {
   int sum = 0;
-  for (int i = start; i < end; i++) sum++;  // For loop to be able to time it
+  for (int i = start; i <= end; i++) sum += i;
   return sum;
 }
 
@@ -22,18 +22,36 @@ static PyObject *interval_counter_wrapper(PyObject *self, PyObject *args) {
 }
 
 
+PyDoc_STRVAR(interval_counter_doc,
+             "interval_counter(start, end, /)\n"
+             "--\n\n"
+             "Compute the sum of the integers in the interval [start, end].\n"
+             "\n"
+             "Args:\n"
+             "    start (int): Start of the interval.\n"
+             "    end (int): End of the interval.\n"
+             "\n"
+             "Returns:\n"
+             "    sum (int): The sum of the integers in the interval: (end-start+1)(end+start)/2.\n"
+             "\n"
+             "Example:\n"
+             "    >>> from mycounter import interval_counter\n"
+             "    >>> interval_counter(2, 20)\n"
+             "    >>> 209\n"
+             );
+
+
 static PyMethodDef CounterMethods[] = {
-  {"interval_counter",  interval_counter_wrapper, METH_VARARGS, "Returns number of numbers in the interval."},
+  {"interval_counter",  interval_counter_wrapper, METH_VARARGS, interval_counter_doc},
   {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
 
 static struct PyModuleDef mycounter_module = {
   PyModuleDef_HEAD_INIT,
-  "mycounter",   /* name of module */
-  NULL, /* module documentation, may be NULL */
-  -1,       /* size of per-interpreter state of the module,
-               or -1 if the module keeps state in global variables. */
+  "mycounter",   /* Name of module */
+  "Inhouse c python package",  /* Module documentation, may be NULL */
+  -1,       /* Size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
   CounterMethods
 };
 
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import Extension, setup
+# from setuptools import Extension, setup
 
-module1 = Extension("mycounter", sources=["interval_counter/interval_counter.c"])
+counter_module = Extension("mycounter", sources=["interval_counter/interval_counter.c"])
 
-setup(name="mycounter",
-      version="1.0.0",
-      description="My test package",
-      ext_modules=[module1])
+setup(
+    name="mycounter",
+    version="0.0.1",
+    description="Interval counter package",
+    ext_modules=[counter_module])
