f0529823809ebbe6ace81a2d0ad6d9383bf9ac1f
Hoel Bagard <hoel.bagard@gmail.com>
2022-04-21 14:16:00 +0900

    Updated test file.

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,19 @@
-from mycounter import interval_counter
+from inhouse import interval_counter, list_append_1, print_hello_world
 
 
 def main():
-    print("Start")
-    sum_res = interval_counter(1, 10)
-    print(f"Finished properly, result: {sum_res}")
+    print("Test script starting")
+
+    print_hello_world()
+
+    sum_res = interval_counter(start:=1, end:=10)
+    print(f"The sum of the integers in the interval [{start}, {end}] is: {sum_res}")
+
+    my_list = [3, "2"]
+    print(f"List before: {my_list}")
+    list_append_1(my_list)
+    print(f"List after: {my_list}")
+    print("Finished properly")
 
 
 if __name__ == "__main__":
